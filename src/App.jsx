0d7fac0faadcdde38035fc60b4b89b178bd312fa
raj-subhankar <subhankar.rj@gmail.com>
2021-08-27 11:25:12 +0530

    Implement NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    document.title = "Not Found - Instagram";
+  }, []);
+
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.LOGIN} component={Login} exact />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
+          <Route component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
   );
 }
-
-export default App;

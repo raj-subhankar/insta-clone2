50ed00cfb319654551a7521b3ff3e01889925d99
raj-subhankar <subhankar.rj@gmail.com>
2021-08-29 12:49:22 +0530

    Create sidebar user

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -8,8 +8,8 @@ export default function Sidebar() {
   } = useUser();
   return (
     <div className="p-4">
-      <User />
-      <Suggestions />
+      <User username={username} fullName={fullName} />
+      <Suggestions userId={userId} />
     </div>
   );
 }
--- src/components/sidebar/user.jsx
+++ src/components/sidebar/user.jsx
@@ -1,3 +1,33 @@
-export default function User() {
-  return <p>I am a user</p>;
-}
+import PropTypes from "prop-types";
+import Skeleton from "react-loading-skeleton";
+
+const { Link } = require("react-router-dom");
+
+const User = ({ username, fullName }) =>
+  !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link
+      to={`/p/${username}`}
+      className="grid grid-cols-4 gap-4 mb-6 items-center"
+    >
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src="/images/avatars/karl.jpg"
+          alt="User profile"
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
+    </Link>
+  );
+
+export default User;
+
+User.propTypes = {
+  username: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired,
+};

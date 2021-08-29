96c580079f0ea15b4bafc9a5526f3b0501c92aa9
raj-subhankar <subhankar.rj@gmail.com>
2021-08-29 20:44:56 +0530

    Create Suggested profiles

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -4,12 +4,12 @@ import User from "./user";
 
 export default function Sidebar() {
   const {
-    user: { fullName, username, userId },
+    user: { fullName, username, userId, following },
   } = useUser();
   return (
     <div className="p-4">
       <User username={username} fullName={fullName} />
-      <Suggestions userId={userId} />
+      <Suggestions userId={userId} following={following} />
     </div>
   );
 }

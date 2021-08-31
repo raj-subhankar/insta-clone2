adf1dd6553c9ee3b8d9eb6b31429098aaec01b59
raj-subhankar <subhankar.rj@gmail.com>
2021-08-31 21:31:27 +0530

    Create custom hook to fetch posts

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,4 +1,8 @@
+import usePhotos from "../hooks/use-photos";
+
 export default function Timeline() {
+  const { photos } = usePhotos();
+  console.log("photos", photos);
   return (
     <div className="container col-span-2">
       <p>I am the timeline</p>
